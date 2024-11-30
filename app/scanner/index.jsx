8397700e6f1759de157aa6b944457bd590c0e904
Bhavin <bhavinkhunt71@gmail.com>
2024-11-30 11:03:03 +0530

    ..

--- app/scanner/index.jsx
+++ app/scanner/index.jsx
@@ -8,12 +8,20 @@ import {
   Share,
   Clipboard,
   Linking,
+  useColorScheme,
+  PermissionsAndroid,
 } from "react-native";
-import { useRouter, useLocalSearchParams } from "expo-router";
+import { useLocalSearchParams, useNavigation } from "expo-router";
 import WifiManager from "react-native-wifi-reborn";
 import * as Calendar from "react-native-add-calendar-event";
 import Icon from "react-native-vector-icons/MaterialIcons";
+import { StatusBar } from "expo-status-bar";
 
+function capitalizeFirstWord(string) {
+  if (!string) return "QR Code";
+  if (string == "raw") return "QR Code";
+  return string.charAt(0).toUpperCase() + string.slice(1);
+}
 const parseDate = (dateStr) => {
   const year = dateStr.substring(0, 4);
   const month = dateStr.substring(4, 6) - 1; // Months are 0-indexed in JS Date
@@ -25,30 +33,28 @@ const parseDate = (dateStr) => {
 };
 
 export default function QrDataDisplay() {
-  // const router = useRouter();
   const [data, setData] = useState();
-  const { rawData } = useLocalSearchParams();
+  const { rawData,realData } = useLocalSearchParams();
+  const navigation = useNavigation();
+  const colorScheme = useColorScheme();
   let dData;
+
+
   const parseQRCodeData = (data) => {
     if (data.startsWith("http://") || data.startsWith("https://")) {
-      dData = {
-        Url: data,
-      };
+      dData = { Url: data };
       return { type: "url", content: data };
     } else if (data.startsWith("WIFI:")) {
       const ssid = data.match(/S:(.*?);/)[1];
       const password = data.match(/P:(.*?);/)[1];
-      dData = {
-        ssid: ssid,
-        password: password,
-      };
+      dData = { ssid, password };
       return { type: "wifi", ssid, password };
     } else if (data.startsWith("BEGIN:VEVENT")) {
       const title = data.match(/SUMMARY:(.*?)(?:\n|$)/)[1];
       let startDate = data.match(/DTSTART:(.*?)(?:\n|$)/)[1];
       let endDate = data.match(/DTEND:(.*?)(?:\n|$)/)[1];
       dData = {
-        title: title,
+        title,
         startDate: parseDate(startDate).toLocaleString(),
         endDate: parseDate(endDate).toLocaleString(),
       };
@@ -57,42 +63,40 @@ export default function QrDataDisplay() {
       return { type: "calendar", title, startDate, endDate };
     } else if (data.startsWith("mailto:")) {
       const email = data.replace("mailto:", "").trim();
-      dData = {
-        email: email,
-      };
+      dData = { email };
       return { type: "email", email };
     } else if (data.startsWith("tel:")) {
       const phone = data.replace("tel:", "").trim();
-      dData = {
-        phone: phone,
-      };
+      dData = { phone };
       return { type: "phone", phone };
     } else if (data.startsWith("SMSTO:")) {
       const [phone, message] = data.replace("SMSTO:", "").split(":");
-      dData = {
-        phone: phone,
-        message: message,
-      };
+      dData = { phone, message };
       return { type: "sms", phone, message };
     }
-    dData = {
-      data,
-    };
+    dData = { data };
     return { type: "raw", content: data };
   };
 
   const qrData = parseQRCodeData(rawData);
   useEffect(() => {
+    navigation.setOptions({
+      title: `${capitalizeFirstWord(qrData.type)}`,
+      headerStyle: {
+        backgroundColor: "#8c11e4",
+      },
+      headerTintColor: "#FFF",
+    });
+
     setData(dData);
   }, [rawData]);
+
   const addEventToCalendar = async (title, startDate, endDate) => {
     try {
-      // const start = parseDate(startDate);
-      // const end = parseDate(endDate);
       const eventConfig = {
         title,
-        startDate: startDate,
-        endDate: endDate,
+        startDate,
+        endDate,
         notes: "QRScanner",
       };
 
@@ -141,19 +145,33 @@ export default function QrDataDisplay() {
         break;
       case "share":
         await Share.share({
-          message: content || title || email || phone || message,
+          message: realData
         });
         break;
       case "connectWifi":
         if (type === "wifi") {
           try {
-            await WifiManager.connectToProtectedSSID(
-              ssid,
-              password,
-              false,
-              false
+            const granted = await PermissionsAndroid.request(
+              PermissionsAndroid.PERMISSIONS.ACCESS_FINE_LOCATION,
+              {
+                title: "Location permission is required for WiFi connections",
+                message:
+                  "This app needs location permission as this is required  " +
+                  "to scan for wifi networks.",
+                buttonNegative: "DENY",
+                buttonPositive: "ALLOW",
+              }
             );
-            Alert.alert("Connected", `Successfully connected to ${ssid}`);
+            if (granted) {
+              await WifiManager.connectToProtectedSSID(
+                ssid,
+                password,
+                false,
+                false
+              );
+
+              Alert.alert("Connected", `Successfully connected to ${ssid}`);
+            }
           } catch (error) {
             Alert.alert(
               "Error",
@@ -188,6 +206,15 @@ export default function QrDataDisplay() {
           );
         }
         break;
+      case "searchWeb":
+        if (type === "raw") {
+          Linking.openURL(
+            `https://www.google.com/search?q=${encodeURIComponent(content)}`
+          ).catch((err) =>
+            Alert.alert("Error", `Failed to search the web: ${err.message}`)
+          );
+        }
+        break;
       default:
         break;
     }
@@ -198,73 +225,120 @@ export default function QrDataDisplay() {
       return (
         <View style={styles.dataItem}>
           {Object.entries(data).map(([key, value]) => (
-            <Text key={key} style={styles.text}>{value}</Text>
+            <Text
+              key={key}
+              style={[
+                styles.text,
+                { color: colorScheme === "light" ? "#121212" : "#FFFFFF" },
+              ]}
+            >
+              {value}
+            </Text>
           ))}
         </View>
       );
     }
   };
 
-  console.log(data);
   return (
-    <View style={styles.container}>
+    <View
+      style={[
+        styles.container,
+        { backgroundColor: colorScheme === "dark" ? "#121212" : "#FFFFFF" },
+      ]}
+    >
+      <StatusBar backgroundColor="#8c11e4" style="light" />
+      {/* Header */}
+      {/* <View style={styles.header}>
+        <Text
+          style={[
+            styles.headerText,
+            { color: colorScheme === "dark" ? "#BB86FC" : "#8c11e4" },
+          ]}
+        >
+          QR Code Type: {qrData.type || "No Data"}
+        </Text>
+        <Text
+          style={[
+            styles.detailsText,
+            { color: colorScheme === "dark" ? "#FFFFFF" : "#000000" },
+          ]}
+        >
+          {JSON.stringify(data, null, 2)}
+        </Text>
+      </View> */}
+      {/* Content */}
       <View style={styles.dataContainer}>
-        {qrData.type === "url" && (
-          <Icon name="link" size={40} color="#007BFF" />
-        )}
-        {qrData.type === "wifi" && (
-          <Icon name="wifi" size={40} color="#007BFF" />
-        )}
+        {/* {qrData.type === "url" && <Icon name="link" size={40} color="#8c11e4" />}
+        {qrData.type === "wifi" && <Icon name="wifi" size={40} color="#8c11e4" />}
         {qrData.type === "calendar" && (
-          <Icon name="event" size={40} color="#007BFF" />
+          <Icon name="event" size={40} color="#8c11e4" />
         )}
         {qrData.type === "email" && (
-          <Icon name="email" size={40} color="#007BFF" />
+          <Icon name="email" size={40} color="#8c11e4" />
         )}
         {qrData.type === "phone" && (
-          <Icon name="phone" size={40} color="#007BFF" />
+          <Icon name="phone" size={40} color="#8c11e4" />
         )}
-        {qrData.type === "sms" && <Icon name="sms" size={40} color="#007BFF" />}
-        {/* <Text style={styles.text}>{JSON.stringify(data, null, 2)}</Text> */}
+        {qrData.type === "sms" && <Icon name="sms" size={40} color="#8c11e4" />}
+        {qrData.type === "raw" && (
+          <Icon name="search" size={40} color="#8c11e4" />
+        )} */}
         {renderQRContent()}
-      </View>
-      <View style={styles.buttonRow}>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => handleAction("copy")}
-        >
-          <Text style={styles.buttonText}>Copy</Text>
-        </TouchableOpacity>
-        <TouchableOpacity
-          style={styles.button}
-          onPress={() => handleAction("share")}
-        >
-          <Text style={styles.buttonText}>Share</Text>
-        </TouchableOpacity>
-        {qrData.type === "url" && (
+
+        {/* Buttons */}
+        <View style={styles.buttonRow}>
+          {qrData.type === "url" && (
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#8c11e4" }]}
+              onPress={() => handleAction("open")}
+            >
+              <Icon name="open-in-browser" size={20} color="#fff" />
+              <Text style={styles.buttonText}>Open</Text>
+            </TouchableOpacity>
+          )}
+          {qrData.type === "wifi" && (
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#8c11e4" }]}
+              onPress={() => handleAction("connectWifi")}
+            >
+              <Icon name="wifi" size={20} color="#fff" />
+              <Text style={styles.buttonText}>Connect</Text>
+            </TouchableOpacity>
+          )}
+          {qrData.type === "calendar" && (
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#8c11e4" }]}
+              onPress={() => handleAction("addCalendar")}
+            >
+              <Icon name="event" size={20} color="#fff" />
+              <Text style={styles.buttonText}>Add Event</Text>
+            </TouchableOpacity>
+          )}
+          {qrData.type === "raw" && (
+            <TouchableOpacity
+              style={[styles.button, { backgroundColor: "#8c11e4" }]}
+              onPress={() => handleAction("searchWeb")}
+            >
+              <Icon name="search" size={20} color="#fff" />
+              <Text style={styles.buttonText}>Search Web</Text>
+            </TouchableOpacity>
+          )}
           <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleAction("open")}
+            style={[styles.button, { backgroundColor: "#8c11e4" }]}
+            onPress={() => handleAction("copy")}
           >
-            <Text style={styles.buttonText}>Open</Text>
+            <Icon name="content-copy" size={20} color="#fff" />
+            <Text style={styles.buttonText}>Copy</Text>
           </TouchableOpacity>
-        )}
-        {qrData.type === "wifi" && (
           <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleAction("connectWifi")}
+            style={[styles.button, { backgroundColor: "#8c11e4" }]}
+            onPress={() => handleAction("share")}
           >
-            <Text style={styles.buttonText}>Connect</Text>
+            <Icon name="share" size={20} color="#fff" />
+            <Text style={styles.buttonText}>Share</Text>
           </TouchableOpacity>
-        )}
-        {qrData.type === "calendar" && (
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleAction("addCalendar")}
-          >
-            <Text style={styles.buttonText}>Add Event</Text>
-          </TouchableOpacity>
-        )}
+        </View>
       </View>
     </View>
   );
@@ -273,24 +347,47 @@ export default function QrDataDisplay() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
     padding: 20,
-    backgroundColor: "#FFFFFF",
   },
-  dataContainer: { alignItems: "center", marginBottom: 20 },
-  text: { fontSize: 16, marginVertical: 10, textAlign: "center" },
-  dataItem: { marginVertical: 5 },
+  header: {
+    position: "absolute",
+    top: 10,
+    left: 10,
+  },
+  headerText: {
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  detailsText: {
+    marginTop: 5,
+    fontSize: 14,
+  },
+  dataContainer: {
+    flex: 1,
+  },
+  dataItem: {
+    marginBottom: 14,
+  },
+  text: {
+    fontSize: 16,
+    marginVertical: 2,
+    // textAlign: "center",
+  },
   buttonRow: {
     flexDirection: "row",
     flexWrap: "wrap",
     justifyContent: "center",
   },
   button: {
-    backgroundColor: "#007BFF",
+    flexDirection: "row",
+    alignItems: "center",
     padding: 10,
     borderRadius: 5,
     margin: 5,
   },
-  buttonText: { color: "#fff", fontSize: 16 },
+  buttonText: {
+    color: "#fff",
+    fontSize: 16,
+    marginLeft: 5,
+  },
 });
